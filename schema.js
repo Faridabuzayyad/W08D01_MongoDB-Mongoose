3c95cce5185ea6cd69833e211e0bf996090fa7d7
Faridabuzayyad <farid.abuzayyad@gmail.com>
2021-05-17 18:07:17 +0300

    Pulse Check Q4 - Done

--- schema.js
+++ schema.js
@@ -1,1 +1,11 @@
 const mongoose = require("mongoose");
+
+const todoSchema = new mongoose.Schema({
+    task: {type: String , required: true , unique: true},
+    description: {type: String},
+    deadline: {type: Date},
+    isCompleted: {type: Boolean},
+    priority: {type: Number},
+});
+
+module.exports = mongoose.model("Todo", todoSchema);
