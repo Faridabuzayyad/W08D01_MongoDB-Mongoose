fcafbd4eee544935f184b93afeb5650b58e27248
Faridabuzayyad <farid.abuzayyad@gmail.com>
2021-05-17 18:32:03 +0300

    Practice Q1 - Done

--- app.js
+++ app.js
@@ -5,10 +5,24 @@ const db = require("./db");
 const app = express();
 app.use(express.json());
 
+
+
 app.get("/todos", (req, res) => {});
-app.post("/create/todo", (req, res) => {});
+app.post("/create/todo", (req, res) => {
+    const {task, description, deadline, isCompleted, priority} = req.body;
+    const todo = new todoModel({task, description, deadline, isCompleted, priority});
+    try {
+        const todoResult = await todo.save();
+        res.json(todoResult);
+    } catch (error) {
+        res.json(error);
+    }
+});
 app.put("/update/todo", (req, res) => {});
 app.delete("/delete/todo", (req, res) => {});
+
+
+
 
 const port = 3000;
 app.listen(port, () => {
